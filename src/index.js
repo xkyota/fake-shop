93478c3e2fe6e7dff1f6001e986659ca8a8e3d6f
xkyota <mdurihin@gmail.com>
2025-08-05 20:43:25 +0300

    Error

--- src/index.js
+++ src/index.js
@@ -49,13 +49,33 @@ async function fetchPosts() {
 
 async function loadAndRenderPosts() {
 	try {
-		const posts = await fetchPosts();
-		renderPosts(posts);
-		page += 1;
+		const spinnerHTML = `
+			<div class="spinner-border" role="status" id="spinner">
+			</div>
+		`;
 
-		if (page > 1) {
-			loadMoreBtn.textContent = "Load more products";
-		}
+		loadMoreBtn.style.display = 'none'; 
+
+		// const LoadMoreHTML = `<button class="load-more">Load More</button>`;
+
+		// if (loadMoreBtn) loadMoreBtn.remove();
+
+		const productsSection = document.querySelector(".products");
+		productsSection.insertAdjacentHTML("beforeend", spinnerHTML);
+
+		const posts = await fetchPosts();
+
+		setTimeout(() => {
+			const spinner = document.getElementById("spinner");
+			if (spinner) spinner.remove();
+			renderPosts(posts);
+			page += 1;
+
+			loadMoreBtn.style.display = 'none'; 
+
+			// productsSection.insertAdjacentHTML("beforeend", LoadMoreHTML);
+		}, 1000);
+
 	} catch (error) {
 		console.error("Error fetching posts:", error);
 	}
@@ -75,7 +95,9 @@ productsList.addEventListener("click", (event) => {
 		const productId = event.target.dataset.id;
 		const productItem = event.target.closest(".product-item");
 		const title = productItem.querySelector("h3").textContent;
-		const price = parseFloat(productItem.querySelector("p").textContent.replace("$", ""));
+		const price = parseFloat(
+			productItem.querySelector("p").textContent.replace("$", "")
+		);
 		const image = productItem.querySelector("img").src;
 		const description = productItem.querySelector("span").textContent;
 
